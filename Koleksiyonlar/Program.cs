265d12ab4a04345affd412412be6391ba27eb2a3
firatengin404 <62977294+firat404engin@users.noreply.github.com>
2024-09-08 19:15:37 +0300

    08.09.2024 Ders Sonu

---
+++ Koleksiyonlar/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Documents;
+
+namespace Koleksiyonlar
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			//string[] isimler = new string[] {"firat","engin","gamze","demir" };
+			//Console.WriteLine(isimler[0]);
+			//Console.WriteLine(isimler[1]);
+			//Console.WriteLine(isimler[2]);
+			//Console.WriteLine(isimler[3]);
+
+
+			List<string> isimler2 = new List<string>{"gamze", "demir"};
+			Console.WriteLine(isimler2[0]);
+			Console.WriteLine(isimler2[1]);
+			isimler2.Add("firat");
+			isimler2.Add("engin");
+			Console.WriteLine(isimler2[2]);
+			Console.WriteLine(isimler2[1]);
+
+			Console.ReadLine();
+		}
+	}
+}
---
+++ OOP1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP1
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			Product product1 = new Product();
+			product1.Id = 1;
+			product1.CategoryId = 2;
+			product1.ProductName = "Masa";
+			product1.UnitPrice = 500;
+			product1.UnitsInStock = 3;
+
+			Product product2 = new Product {Id=2,CategoryId=3,
+				ProductName="Masa", UnitPrice=5,
+				UnitsInStock=73 };
+
+			// PascalCase    // camelCase
+			// case sensitive
+
+			// int , double , bool değer tip
+			// diziler , class , interface, abstract  referans tip
+
+
+			ProductManager productManeger = new ProductManager();
+			productManeger.topla2(2, 3);
+			Console.ReadLine();
+		}
+	}
+}
